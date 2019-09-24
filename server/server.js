04d08444655d1a964429f7806405c32345adb527
Ivan Mersich <ivanmersich@gmail.com>
2019-09-23 22:31:21 -0300

    sin express

--- server/server.js
+++ server/server.js
@@ -1,8 +1,12 @@
 require('./config/config');
 
 const express = require('express');
-const app = express();
-
+//const app = express();
+const http = require('http')
+const app = http.createServer((req, res)=> {
+    res.statusCode =200
+})
+/*
 const bodyParser = require('body-parser');
 
 // parse application/x-www-form-urlencoded
@@ -11,25 +15,46 @@ app.use(bodyParser.urlencoded({ extended: false }))
 // parse application/json
 app.use(bodyParser.json())
 
+const EventEmitter = require('events');
+
+class MyEmitter extends EventEmitter {}
+
+const myEmitter = new MyEmitter();
+myEmitter.on('event', () => {
+  console.log('an event occurred!');
+});
+//myEmitter.emit('event');
+myEmitter.emit('event', () => {
+    console.log('un evento se ha emitido');
+  });
+*/
 app.on('request', (req, res) => {
-    console.log("esta es la req"+req)
+    console.log("esta es la request: "+"url: "+req.url+"  metodo: "+req.method)
     console.log("esta es la res"+res)
 });
 app.on('upgrade', (req, res) => {
-    console.log("esta es la req"+req)
+    console.log("esta es la req up"+req.url)
     console.log("esta es la res"+res)
 });
 app.on('checkContinue', (req, res) => {
-    console.log("esta es la req"+req)
+    console.log("esta es la req che"+req.url)
     console.log("esta es la res"+res)
 });
 app.on('clientError', (req, res) => {
-    console.log("esta es la req"+req)
+    console.log("esta es la req cli"+req.url)
     console.log("esta es la res"+res)
 });
+/*
 var gget=0
 var ppost=0
 var pput=0
+var ccon=0
+
+app.on('request', function(req, res) {
+    ccon=ccon+1
+    console.log("llego una conexion nueva!!!! Nro: "+ccon)
+    res.json('Conexion !!! Nro: '+ccon);
+});
 
 app.get('/*', function(req, res) {
     gget=gget+1
@@ -89,7 +114,7 @@ app.delete('/usuario', function(req, res) {
     console.log("llego una peticion DELETE !!!!")
     res.json('delete Usuario');
 });
-
+*/
 app.listen(process.env.PORT, () => {
     console.log('Escuchando puerto: ', process.env.PORT);
 });
